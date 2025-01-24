b5c31873dbf8e4156dfd8b2b928e9ddebee9e71d
ian <rubiefawn@gmail.com>
2025-01-24 15:00:08 -0700

    Improve usage info for swww-randomize.sh

--- swww-randomize.sh
+++ swww-randomize.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-# This script will choose a random file from a given directory at regular intervals and set it as the wallpaper
+# Changes the wallpaper to a randomly chosen image in a given directory at a set
+# interval.
 
-if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
-	echo "Usage:
-	$0 <dir containing images> [interval in seconds]"
+if [ $# -lt 1 ] || [ ! -d "$1" ]; then
+	printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
+	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every INTERVAL seconds (or every 600 seconds if unspecified)."
 	exit 1
 fi
 
-export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION_STEP=2
+export SWWW_TRANSITION_FPS="${SWWW_TRANSITION_FPS:-60}"
+export SWWW_TRANSITION_STEP="${SWWW_TRANSITION_STEP:-2}"
 
 while true; do
 	find "$1" -type f \
@@ -18,16 +19,6 @@ while true; do
 	| sort -n | cut -d':' -f2- \
 	| while read -r img; do
 		swww img "$img"
-		sleep ${2:-600} # Default interval of 600s if not specified
+		sleep "${2:-600}" # Default interval of 600s if not specified
 	done
 done
-
-# The following is cleaner, but does not work on Busybox, since it
-# doesn't have sort -R
-
-# while true; do
-# 	find "$1" -type f | sort -R | while read -r img; do
-# 		swww img "$img"
-# 		sleep $INTERVAL
-# 	done
-# done
