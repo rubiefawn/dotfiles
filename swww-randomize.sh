99b60967a55ecacab73ed42ab8568c8e05dddc7a
ian <rubiefawn@gmail.com>
2025-01-24 17:10:30 -0700

    Add multi-monitor support to swww-randomize.sh

--- swww-randomize.sh
+++ swww-randomize.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
-# Changes the wallpaper to a randomly chosen image in a given directory at a set
-# interval.
+# For each display, changes the wallpaper to a randomly chosen image in
+# a given directory at a set interval.
+
+DEFAULT_INTERVAL=300 # In seconds
 
 if [ $# -lt 1 ] || [ ! -d "$1" ]; then
 	printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
-	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every INTERVAL seconds (or every 600 seconds if unspecified)."
+	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified)." "$DEFAULT_INTERVAL"
 	exit 1
 fi
 
+# See swww-img(1)
+RESIZE_TYPE="fit"
 export SWWW_TRANSITION_FPS="${SWWW_TRANSITION_FPS:-60}"
 export SWWW_TRANSITION_STEP="${SWWW_TRANSITION_STEP:-2}"
 
@@ -18,7 +22,13 @@ while true; do
 	done \
 	| sort -n | cut -d':' -f2- \
 	| while read -r img; do
-		swww img "$img"
-		sleep "${2:-600}" # Default interval of 600s if not specified
+		for d in $(swww query | grep -Po "^[^:]+"); do # see swww-query(1)
+			# Get next random image for this display, or re-shuffle images
+			# and pick again if no more unused images are remaining
+			[ -z "$img" ] && read -r img || break 2
+			swww img --resize "$RESIZE_TYPE" --outputs "$d" "$img"
+			unset -v $img
+		done
+		sleep "${2:-$DEFAULT_INTERVAL}"
 	done
 done
