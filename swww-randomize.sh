055ba9d6a0dba239182e5016c7d3f11842fd5246
Fawn Sannar <rubiefawn@gmail.com>
2025-02-19 22:28:32 -0700

    Fix swww-randomize.sh
    - Fix eternal hang due to silly shell syntax
    - Fix usage message formatting

--- swww-randomize.sh
+++ swww-randomize.sh
@@ -6,12 +6,12 @@ DEFAULT_INTERVAL=300 # In seconds
 
 if [ $# -lt 1 ] || [ ! -d "$1" ]; then
 	printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
-	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified)." "$DEFAULT_INTERVAL"
+	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified).\n" "$DEFAULT_INTERVAL"
 	exit 1
 fi
 
 # See swww-img(1)
-RESIZE_TYPE="fit"
+RESIZE_TYPE="crop"
 export SWWW_TRANSITION_FPS="${SWWW_TRANSITION_FPS:-60}"
 export SWWW_TRANSITION_STEP="${SWWW_TRANSITION_STEP:-2}"
 
@@ -25,9 +25,9 @@ while true; do
 		for d in $(swww query | grep -Po "^[^:]+"); do # see swww-query(1)
 			# Get next random image for this display, or re-shuffle images
 			# and pick again if no more unused images are remaining
-			[ -z "$img" ] && read -r img || break 2
+			[ -z "$img" ] && if read -r img; then true; else break 2; fi
 			swww img --resize "$RESIZE_TYPE" --outputs "$d" "$img"
-			unset -v $img
+			unset -v img
 		done
 		sleep "${2:-$DEFAULT_INTERVAL}"
 	done
