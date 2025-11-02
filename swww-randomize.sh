ce01fce6ba5b23381e49d7b84a4dacdb441f2f71
Fawn Sannar <rubiefawn@gmail.com>
2025-11-02 15:32:38 -0700

    Update wallpaper randomization script

--- swww-randomize.sh
+++ swww-randomize.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# https://codeberg.org/LGFae/awww/src/branch/main/example_scripts/awww_randomize_multi.sh
 # For each display, changes the wallpaper to a randomly chosen image in
 # a given directory at a set interval.
 
@@ -6,14 +7,17 @@ DEFAULT_INTERVAL=300 # In seconds
 
 if [ $# -lt 1 ] || [ ! -d "$1" ]; then
 	printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
-	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified).\n" "$DEFAULT_INTERVAL"
+	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified)." "$DEFAULT_INTERVAL"
 	exit 1
 fi
 
-# See swww-img(1)
+# See awww-img(1)
 RESIZE_TYPE="crop"
-export SWWW_TRANSITION_FPS="${SWWW_TRANSITION_FPS:-60}"
-export SWWW_TRANSITION_STEP="${SWWW_TRANSITION_STEP:-2}"
+export AWWW_TRANSITION_FPS="${AWWW_TRANSITION_FPS:-60}"
+export AWWW_TRANSITION_STEP="${AWWW_TRANSITION_STEP:-2}"
+
+# TODO: Remove alias once rename occurs in distro packages
+alias awww="swww"
 
 while true; do
 	find "$1" -type f \
@@ -22,12 +26,12 @@ while true; do
 	done \
 	| sort -n | cut -d':' -f2- \
 	| while read -r img; do
-		for d in $(swww query | grep -Po "^[^:]+"); do # see swww-query(1)
+		for d in $(awww query | awk '{print $2}' | sed s/://); do # see awww-query(1)
 			# Get next random image for this display, or re-shuffle images
 			# and pick again if no more unused images are remaining
 			[ -z "$img" ] && if read -r img; then true; else break 2; fi
-			swww img --resize "$RESIZE_TYPE" --outputs "$d" "$img"
-			unset -v img
+			awww img --resize "$RESIZE_TYPE" --outputs "$d" "$img"
+			unset -v img # Each image should only be used once per loop
 		done
 		sleep "${2:-$DEFAULT_INTERVAL}"
 	done
